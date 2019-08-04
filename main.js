75e2fe0580edb44d8fe9de008c0253ad4b82263e
TahaAlTamimi <altamimitmo@gmail.com>
2019-08-04 10:52:15 +0300

    finish assesment

--- main.js
+++ main.js
@@ -36,15 +36,27 @@ var object2={
 */
 
 //1) WRITE YOUR CODE UNDER THIS LINE         
-
+// x=>(x,y)=x+y
 //2) WRITE YOUR CODE UNDER THIS LINE         
+// consolereturn=>(x,y)=y
+//3) WRITE YOUR CODE UNDER THIS LINE 
+// let name ="alex";
+// let age=25;        
+// x=>(name,age)="My name is"+name+"and my age is"+age;
+// i remember  to use some thing can do all we need with tags to give user like <h1>blabla ${nameo or"what you want}</h1>
+// but must use "let "
 
-//3) WRITE YOUR CODE UNDER THIS LINE         
 
 //4) WRITE YOUR CODE UNDER THIS LINE         
+// let food ="fried chicken";
+// let color="Blue";
+// object=>{
+//   food,
+//   color
+// };
 
 //5) WRITE YOUR CODE UNDER THIS LINE         
-
+// mul=>(a,b)=a*b;
 
 
 
@@ -73,6 +85,16 @@ Output =>
 */
 
 // WRITE YOUR CODE UNDER THIS LINE
+// class Computer {
+//   constructor{OS,RAM,CPU){
+//     OS=this .OS
+//     RAM=this.RAM
+//     CPU=this.CPU
+//   }
+// doubleRAm:f=>obj{OS,RAM,CPU}
+//   }
+  // there is error in synatax in "constructor" must be using this and and () in constructor.
+
 
 
 
@@ -86,6 +108,8 @@ please fix the errors inside them
 */
 
 // App Component
+import React ,{ Component } from "react";
+import App from '.App';
 import Tasks from './components/Tasks';
 
 export default class App extends Component {
@@ -94,26 +118,28 @@ export default class App extends Component {
     todos: ['eat', 'eat eat', 'eact again']
   };
   changeTitle() {
-    state.title = 'AGGREGOR ZOLDYCK'
+  setState.title = 'AGGREGOR ZOLDYCK'
   }
   render() {
     return (
+      <div>
       <h1>App Component => state.title</h1>
-      <button onClick={this.changeTitle}>Change Title</button>
+      <button onClick={this.title.changeTitle}>Change Title</button>
       <Tasks tasks={this.todos} changeTitleFromChild={this.changeTitle} />
+      </div>
     );
   }
 }
 
 // Tasks Component
 import React, { Component } from 'react';
-
-class Tasks extends Component {
+import Tasks from './Tasks'
+export default class Tasks extends Component {
   state = {
     day: "Sat"
   };
   changeDay() {
-    day = 'Sun'
+    setState.day = 'Sun'
   }
 
   render() {
@@ -121,7 +147,7 @@ class Tasks extends Component {
       <div>
         <h1>Tasks Component => state.day</h1>
         <button onClick={this.changeDay}>Change Tasks State</button>
-        <button onClick={changeTitle}>Change App State</button>
+        <button onClick={this.props.changeTitleFromChild}>Change App State</button>
       </div>
     );
   }
